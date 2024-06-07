a7bef976a98a187c10f6354003e6c9f627eee0cc
Stephanne Lima <automata.s012@gmail.com>
2024-06-06 23:53:31 -0300

    tarefa feita

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,17 +1,21 @@
-function handleModalUser() {
-  const buttons = document.querySelectorAll('.button_modal');
-
-  const modalController = document.querySelector('#modal');
-  const close = document.querySelector('.modal_close')
-
-  for (let i = 0; i < buttons.length; i++) {
-      buttons[i].addEventListener('click', function () {
-          modalController.showModal();
-      })
+function modalRegister(){
+    const modal = document.querySelector("#modalController");
+    const button = document.querySelectorAll(".showModal");
+    for(let i = 0; i <= button.length; i++){
+    button[i].addEventListener("click", () => {
+        modal.showModal();
+        closeModal()
+        
+    });
   }
-  close.addEventListener('click', function () {
-      modalController.close();
-  })
-}
-
-handleModalUser()
+  }
+  
+  function closeModal(){
+    const button = document.querySelector(".closeModal");
+    const modal = document.querySelector("#modalController");
+    button.addEventListener("click", () => {
+        modal.close();
+    })
+  }
+  
+  modalRegister()
