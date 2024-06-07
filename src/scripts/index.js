e8f1ee5409c998f5afa1586a806d9a8122c1c190
Stephanne Lima <automata.s012@gmail.com>
2024-06-06 22:31:04 -0300

    site institucional generico

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,35 +1,17 @@
-function handleModal() {
-    const button = document.querySelector("#headerButton");
-    const faqButton = document.querySelector("#faqButton");
-    const modalContainer = document.querySelector("#modalContainer");
-  
-    button.addEventListener("click", () => {
-      modalContainer.showModal();    
-    });
-  
-    faqButton.addEventListener("click", () => {
-      modalContainer.showModal();    
-    });
-  
-    modalContainer.addEventListener("close", () => {
-      closeModal();    
-    });
+function handleModalUser() {
+  const buttons = document.querySelectorAll('.button_modal');
+
+  const modalController = document.querySelector('#modal');
+  const close = document.querySelector('.modal_close')
+
+  for (let i = 0; i < buttons.length; i++) {
+      buttons[i].addEventListener('click', function () {
+          modalController.showModal();
+      })
   }
-  
-  function closeModal() {
-    const buttonClose = document.querySelector("#closeModal");
-    const modalContainer = document.querySelector("#modalContainer");
-  
-    buttonClose.addEventListener("click", () => {
-      modalContainer.close();
-    });
-  }
-  
-  handleModal();
-  closeModal();
-  
-  modalContainer.addEventListener("click", (event) => {
-      if (event.target === modalContainer) {
-        closeModal();
-      }
-    });
+  close.addEventListener('click', function () {
+      modalController.close();
+  })
+}
+
+handleModalUser()
